e06e06b6498206a81464a46046e6ceed3cdbc866
Jared Arkel Rosas Garcia <arkeljared@gmail.com>
2025-04-11 15:30:07 -0600

    Update faces-model.py

--- faces-model.py
+++ faces-model.py
@@ -6,22 +6,27 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 import cv2
 import os
+import base64
 import pathlib
-import shutil
 
-# Descargar y descomprimir el dataset desde Google Drive
-dataset_url = "https://drive.google.com/uc?export=download&id=1ThUklHdF9_LhujOFvg6-2kxqB79vnO92"
-directory = tf.keras.utils.get_file('losmasguaposdeltec', origin=dataset_url, untar=True)
+# Set the path of the input folder
+
+dataset = "https://drive.google.com/uc?export=download&id=1Y4GEnPImBTBJgdYOS3qVPOeH_5G9y0ns"
+directory = tf.keras.utils.get_file('losmasguaposdeltec.tgz', origin=dataset, untar=True)
 data = pathlib.Path(directory)
 
-# Lista de carpetas/personas esperadas
-listPersons = ['Angel', 'Arkel', 'gabriel', 'Guadalupe', 'JULIO']
+#print(folders)
 
-# Cargar imágenes y etiquetas
+# Import the images and resize them to a 128*128 size
+# Also generate the corresponding labels
+
 labels = []
 images = []
+listPersons = ['angel', 'arkel', 'gabriel', 'guadalupe', 'julio']
 
-print("Carpetas detectadas:")
+size = 64,64
+print('folders')
+#folders.remove("LICENSE.txt")
 print(listPersons)
 
 for personName in listPersons:
@@ -29,31 +34,26 @@ for personName in listPersons:
     for fileName in os.listdir(rostrosPath):
         img_path = os.path.join(rostrosPath, fileName)
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-        if img is not None:
-            img = cv2.resize(img, (150, 150))
-            images.append(img)
-            labels.append(personName)
-        else:
-            print(f"[WARNING] No se pudo leer la imagen: {img_path}")
+        img = cv2.resize(img, (150, 150))
+        images.append(img)
+        labels.append(personName)
 
-# Convertir a numpy array y normalizar
-images = np.array(images).astype('float32') / 255.0
+
+
+# Transform the image array to a numpy type
+
+images = np.array(images)
 labels = np.array(labels)
 
-# Codificar etiquetas
 label_encoder = LabelEncoder()
 labels_encoded = label_encoder.fit_transform(labels)
 
-# Dividir datos en entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(
-    images, labels_encoded, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split(images, labels_encoded, test_size=0.2, random_state=42)
 
-# Dar forma para Keras (agregar canal de grises)
-X_train = X_train.reshape(-1, 150, 150, 1)
-X_test = X_test.reshape(-1, 150, 150, 1)
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 
-# Definir el modelo
+# Develop a sequential model using tensorflow keras
 model = Sequential()
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 1)))
 model.add(MaxPooling2D((2, 2)))
@@ -63,18 +63,12 @@ model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dense(len(listPersons), activation='softmax'))
 
-# Compilar el modelo
+
+# Compute the model parameters
+
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Entrenar el modelo
-model.fit(X_train, y_train, epochs=50, validation_data=(X_test, y_test))
+model.fit(X_train.reshape(-1, 150, 150, 1), y_train, epochs=50, validation_data=(X_test.reshape(-1, 150, 150, 1), y_test))
 
-# Guardar el modelo
 export_path = 'faces-model/1/'
-if os.path.exists(export_path):
-    shutil.rmtree(export_path)  # Eliminar si ya existe para evitar conflictos
-tf.saved_model.save(model, export_path)
-
-# Mostrar etiquetas codificadas
-print("Clases:", label_encoder.classes_)
-print("Etiquetas codificadas:", np.unique(labels_encoded))
+tf.saved_model.save(model, os.path.join('./',export_path))
